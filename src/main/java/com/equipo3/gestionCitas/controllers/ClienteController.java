bf21a4e3ff6aa2ebe0cfe11bee02577e536d042d
Américo González <agzz2003@gmail.com>
2024-11-17 16:46:16 -0600

    Configuracion del repository dentro del controlador cliente

--- src/main/java/com/equipo3/gestionCitas/controllers/ClienteController.java
+++ src/main/java/com/equipo3/gestionCitas/controllers/ClienteController.java
@@ -1,10 +1,15 @@
 package com.equipo3.gestionCitas.controllers;
 
+import com.equipo3.gestionCitas.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //indica que  es un controlador
-@RequestMapping("/api/cliente")//indica la ruta que va a controlar
+@RequestMapping("/api/cliente")//indica la ruta que va a controlar mediante apiRest
 public class ClienteController {
+    @Autowired//hace evitar que secreen nuevos objetos cada vez que se ejute esta clase repositoria
+    private ClienteRepository clienteRepository;// creo un objeto para usarlo
+
 
 }
