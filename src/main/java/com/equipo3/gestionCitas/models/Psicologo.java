f210a6ed04bf1ebf8caf927b5f4fef3fce0459bc
Américo González <agzz2003@gmail.com>
2024-11-17 15:40:46 -0600

    Getters/setters modelo Psicologo

--- src/main/java/com/equipo3/gestionCitas/models/Psicologo.java
+++ src/main/java/com/equipo3/gestionCitas/models/Psicologo.java
@@ -28,4 +28,60 @@ public class Psicologo {
         this.sexo = sexo;
         this.cedulaProfesional = cedulaProfesional;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCedulaProfesional() {
+        return cedulaProfesional;
+    }
+
+    public void setCedulaProfesional(String cedulaProfesional) {
+        this.cedulaProfesional = cedulaProfesional;
+    }
 }
