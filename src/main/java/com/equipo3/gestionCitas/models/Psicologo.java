098e1ef3670217e4849da4018edea3a5c18f1b52
Américo González <agzz2003@gmail.com>
2024-11-17 15:36:17 -0600

    Creacion modelo Psicologo

---
+++ src/main/java/com/equipo3/gestionCitas/models/Psicologo.java
@@ -0,0 +1,11 @@
+package com.equipo3.gestionCitas.models;
+
+public class Psicologo {
+    private Long id;
+    private String nombre;
+    private String telefono ;
+    private int edad;
+    private String correo;
+    private String sexo;
+    private String cedulaProfesional;
+}
