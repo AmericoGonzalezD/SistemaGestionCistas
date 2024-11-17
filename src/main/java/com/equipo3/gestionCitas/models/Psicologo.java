d2c4dceb73b39b5743aecbeaf4fd6c474904005d
Américo González <agzz2003@gmail.com>
2024-11-17 15:39:34 -0600

    Mapeo modelo Psicologo

--- src/main/java/com/equipo3/gestionCitas/models/Psicologo.java
+++ src/main/java/com/equipo3/gestionCitas/models/Psicologo.java
@@ -1,11 +1,19 @@
 package com.equipo3.gestionCitas.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "psicologo")
 public class Psicologo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
     private String telefono ;
     private int edad;
     private String correo;
     private String sexo;
+    @Column(name = "cedula_profesional")
     private String cedulaProfesional;
+
 }
