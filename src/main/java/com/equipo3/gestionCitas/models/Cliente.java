5af819ba619017c807c0c6e5ae260ac624174ccd
Américo González <agzz2003@gmail.com>
2024-11-17 15:23:15 -0600

    Creacion modelo Cliente

---
+++ src/main/java/com/equipo3/gestionCitas/models/Cliente.java
@@ -0,0 +1,12 @@
+package com.equipo3.gestionCitas.models;
+
+public class Cliente {
+    private Long id ;
+    private String nombre ;
+    private String telefono ;
+    private String correo ;
+    private String direccion ;
+    private int edad ;
+    private String sexo;
+    private String estadoCivil ;
+}
