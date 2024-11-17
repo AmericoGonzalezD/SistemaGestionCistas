9b1f132d59ad699a594fdb59662b4f1a2a94e842
Américo González <agzz2003@gmail.com>
2024-11-17 15:32:55 -0600

    Mapeo modelo Cliente y getters/setters

--- src/main/java/com/equipo3/gestionCitas/models/Cliente.java
+++ src/main/java/com/equipo3/gestionCitas/models/Cliente.java
@@ -1,12 +1,83 @@
 package com.equipo3.gestionCitas.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cliente")
 public class Cliente {
-    private Long id ;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;//llave primaria
     private String nombre ;
     private String telefono ;
     private String correo ;
     private String direccion ;
     private int edad ;
     private String sexo;
+    @Column(name="estado_civil")
     private String estadoCivil ;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
 }
