90a037b7700d55d62b8fb2aae9c558161d3f9157
Américo González <agzz2003@gmail.com>
2024-11-17 16:42:10 -0600

    Mapeo del controlador cliente

--- src/main/java/com/equipo3/gestionCitas/controllers/ClienteController.java
+++ src/main/java/com/equipo3/gestionCitas/controllers/ClienteController.java
@@ -1,5 +1,10 @@
 package com.equipo3.gestionCitas.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //indica que  es un controlador
+@RequestMapping("/api/cliente")//indica la ruta que va a controlar
 public class ClienteController {
 
 }
