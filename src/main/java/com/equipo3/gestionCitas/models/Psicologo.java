5b7b7685680cdc7e3a1b5765d8b729d8acc2a779
Américo González <agzz2003@gmail.com>
2024-11-17 22:28:36 -0600

    Modificacion modelo psicologo

--- src/main/java/com/equipo3/gestionCitas/models/Psicologo.java
+++ src/main/java/com/equipo3/gestionCitas/models/Psicologo.java
@@ -11,88 +11,25 @@ public class Psicologo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_psicologo")
     private Long idPsicologo;
-    private String nombre;
-    private String telefono ;
-    private int edad;
-    private String correo;
-    private String sexo;
     @Column(name = "cedula_profesional")
     private String cedulaProfesional;
 
+    // Relación ManyToMany con la tabla Especialidad
     @ManyToMany
     @JoinTable(
-            name = "psicologo_especialidad",
-            joinColumns = @JoinColumn(name = "id_psicologo"),
-            inverseJoinColumns = @JoinColumn(name = "id_especialidad")
+            name = "psicologo_especialidad", // Tabla intermedia
+            joinColumns = @JoinColumn(name = "id_psicologo"), // Llave foránea a Psicologo
+            inverseJoinColumns = @JoinColumn(name = "id_especialidad") // Llave foránea a Especialidad
     )
     private List<Especialidad> especialidades;
+
+    // Relación OneToOne con la tabla Usuario
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "id_psicologo") // Llave foránea que apunta a Usuario
+    private Usuario usuario;
 
     public Psicologo() {
     }
 
-    public Psicologo(Long id, String nombre, String telefono, int edad, String correo, String sexo, String cedulaProfesional) {
-        this.idPsicologo = id;
-        this.nombre = nombre;
-        this.telefono = telefono;
-        this.edad = edad;
-        this.correo = correo;
-        this.sexo = sexo;
-        this.cedulaProfesional = cedulaProfesional;
-    }
-
-    public Long getIdPsicologo() {
-        return idPsicologo;
-    }
-
-    public void setIdPsicologo(Long id) {
-        this.idPsicologo = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getCedulaProfesional() {
-        return cedulaProfesional;
-    }
-
-    public void setCedulaProfesional(String cedulaProfesional) {
-        this.cedulaProfesional = cedulaProfesional;
-    }
 }
